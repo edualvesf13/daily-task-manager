e35fc64e12c379a88ce68368808421cdb21ad74c
Eduardo Ferreira <edualvesf13@gmail.com>
2020-07-13 14:47:38 -0300

    Add functions Drag and drop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,45 @@
-console.log('oiiii')
+const tasks = document.querySelectorAll('#task')
+const dropzones = document.querySelectorAll('#dropzone')
+
+tasks.forEach( task => {
+    task.addEventListener('dragstart', dragstart)
+    task.addEventListener('drag', drag)
+    task.addEventListener('dragend', dragend)
+} )
+
+dropzones.forEach( dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+} )
+
+
+function dragstart(){
+   this.classList.add('is-dragging')
+}
+
+function drag(){
+   
+}
+
+function dragend(){
+    this.classList.remove('is-dragging')
+}
+
+function dragenter(){
+   
+}
+
+function dragover(){
+   const taskBeingDragged = document.querySelector('.is-dragging')
+   this.appendChild(taskBeingDragged)
+}
+
+function dragleave(){
+   
+}
+
+function drop(event){
+   
+}
